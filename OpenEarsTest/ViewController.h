b85d116f33a0bc15f58cffc223868cd3e506c967
James Robertson <robertsonj@atmroberj01.atdom.ad.agilex.com>
2013-06-17 08:54:38 -0400

    Initial Commit

---
+++ OpenEarsTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OpenEarsTest
+//
+//  Created by James Robertson on 6/17/13.
+//  Copyright (c) 2013 James Robertson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
